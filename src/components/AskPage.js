d0d859db5e08109e3521467bc763d0d31b7bb045
BenKer <benediktas.kertenis@codeacademylt.onmicrosoft.com>
2022-07-13 11:30:34 +0300

    AskPage front

---
+++ src/components/AskPage.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import BlueButton from "./BlueButton";
+import Header1 from "./Header1";
+
+const Container = styled.div`
+  margin: 20px;
+`;
+
+const QuestionTitleInput = styled.input`
+  background: none;
+  border: 1px solid #777;
+  border-radius: 3px;
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 10px;
+  margin-bottom: 20px;
+  color: #fff;
+`;
+
+const QuestionBodyTextarea = styled.textarea`
+  background: none;
+  border: 1px solid #777;
+  border-radius: 3px;
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 10px;
+  min-height: 200px;
+  margin-bottom: 20px;
+  color: #fff;
+`;
+export default function AskPage() {
+  return (
+    <Container>
+      <Header1 style={{ marginBottom: "20px" }}>Ask a public question</Header1>
+      <QuestionTitleInput type="text" placeholder="Title of your question" />
+      <QuestionBodyTextarea placeholder="Please describe your question and provide all of the info you can here!" />
+      <BlueButton>Post question</BlueButton>
+    </Container>
+  );
+}
